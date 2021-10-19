0c84920f7b49a69cbf025bba6433846bae0925a0
zailu <919422364@qq.com>
2021-10-19 20:09:05 +0800

    dateTest

--- emailHtml.js
+++ emailHtml.js
@@ -33,7 +33,7 @@ function fn(weatherData, lifeData, word, imgurl, lovingDays) {
         <!-- 天气 -->
         <div>
           <p>
-            <b>今日气温:</b>
+            <b>厦门今日气温:</b>
             <span>${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C</span>
           </p>
           <ul>
