90770c0979b48b83e36c8abf81bcca9457778b5a
zailu <919422364@qq.com>
2021-10-19 17:41:47 +0800

    yml更新

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   pass: 'xkhoaxouqlfjbbif', // 发送者邮箱MTP协议密码
   to: '919422364@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-  location: '101230201',  // 和风天气-成都武侯区的id
+  location: '101230201',  // 和风天气-厦门的id
   type: '1,3,9', // 和风天气-生活指数type
   tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
   startDay: '2016-10-12', // 在一起的日期
