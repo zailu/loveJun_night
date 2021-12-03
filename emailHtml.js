2acf3fb1d9afdecd8950a8f21ae2aab3a1a2d91a
zailu <919422364@qq.com>
2021-12-03 11:16:24 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -11,22 +11,23 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
       </head>
       <body>
       <div style="font-size: 1rem;width: 100%;height: 100%;overflow-y: scroll;box-sizing: border-box;border:1px solid rgba(216, 131, 197, 0.5);">
-        <!-- 天数 -->
-        <div style="font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;position: relative;color: #000000;margin-bottom: 2px;letter-spacing: 0.2rem;">
-			<!-- <p>今天是在一起的第${lovingDays}天！</p>-->
-			<p>结束了第${lovingDays}天对你的思念！</p>
-			<div>
-				今天也要好好睡觉哦
-				<img  style="width: 8%;"; src="https://s3.bmp.ovh/imgs/2021/12/d82c0c74feafd80c.png" />
+			<!-- 天数 -->
+			<div style="font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;position: relative;color: #000000;margin-bottom: 2px;letter-spacing: 0.2rem;">
+				<!-- <p>今天是在一起的第${lovingDays}天！</p>-->
+				<p>结束了第${lovingDays}天对你的思念！</p>
+				<div>
+					今天也要好好睡觉哦
+					<img  style="width: 8%;"; src="https://s3.bmp.ovh/imgs/2021/12/d82c0c74feafd80c.png" />
+				</div>
+				<div style="float:right">
+					<img  style="width: 70%;"; src="https://s3.bmp.ovh/imgs/2021/12/bbb7a7a1173ec12e.png" />
+				</div>
 			</div>
-			<div style="float:right">
-				<img  style="width: 50%;"; src="https://s3.bmp.ovh/imgs/2021/12/bbb7a7a1173ec12e.png" />
+			<!-- 每日一句 -->
+			<div style="float:left;font-size: 1rem;writing-mode:tb-lr;width: 60%;">
+				${content}
 			</div>
-        </div>
-        <!-- 每日一句 -->
-        <div style="font-size: 1rem;text-indent: 2em;font-style: italic;padding: 3rem 2rem;">
-				 ${content}
-        </div>
+			<img  style="width: 100%;"; src="https://s3.bmp.ovh/imgs/2021/12/c31203495969e9f1.png" />
       </div>
       </body>
     </html>
