4992b66821e33310df79d9257af9db04ce26a9fb
zailu <919422364@qq.com>
2021-12-03 10:37:31 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -1,38 +1,59 @@
-function fn(weatherData, lifeData, word, imgurl, lovingDays) {
+function fn(weatherData, lifeData, content, imgurl, lovingDays) {
   const { daily: weatherDataDaily } = weatherData;
   const { daily } = lifeData;
 
   return `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </head>
-    <body>
-      <div>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+      <div style="font-size: 1rem;width: 100%;height: 100%;overflow-y: scroll;box-sizing: border-box;border:1px solid rgba(216, 131, 197, 0.5);">
         <!-- 天数 -->
-        <div>
+        <div style="font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;position: relative;color: #000000;margin-bottom: 2px;letter-spacing: 0.2rem;">
           <!-- <p>今天是在一起的第${lovingDays}天！</p>-->
-		  <p>转眼间一天就过去啦，今天有没有开开心心呢(*^▽^*)</p>
+          <p>结束了第${lovingDays}天对你的思念！</p>
+		  <div>今天有没有开开心心呢</div>
+		  <img  style="width: 50%;"; src="https://s3.bmp.ovh/imgs/2021/12/c78f040792460398.png" />
         </div>
         <!-- 图片 -->
-        <div>
-          <img
-            style="width: 100%; max-width: 768px"
-            src="${imgurl}"
-            alt="图片"
-          />
+        <div style="width: 100%;max-height: 35rem;overflow: hidden;padding: 0 1rem;box-sizing: border-box;border-radius: 0.6rem;position: relative;display: flex;justify-content: center;align-items: center;padding-bottom: 1rem;">
+          <img style="width: 100%;"; src="${imgurl}" alt="图片" />
         </div>
         <!-- 每日一句 -->
-        <div>
-          <p style="font-size: 14px; text-indent: 2em; font-style: italic;color: red;text-align:center">
-            ${word}
-          </p>
+        <div style="font-size: 1rem;text-indent: 2em;font-style: italic;padding: 3rem 2rem;background: url(https://s3.bmp.ovh/imgs/2021/12/b8b68c4a645b0f91.png) no-repeat center top;background-size: 90% 100%;">
+		<!-- <div style="font-size: 1rem;text-indent: 2em;font-style: italic;padding: 1rem 1rem;background: url(https://s3.bmp.ovh/imgs/2021/12/a7bd7218a276cf8a.png) no-repeat center top;background-size: 94% 100%;">	 -->
+			 <div style="padding: 2rem 2rem;font-family: "仿宋";color: #FFFFFF;">
+				 ${content}
+			 </div>
+        </div>
+			<div style="font-weight: bold;color: #000000;padding: 1rem 1rem;">
+				<img style="width: 10%;"; src="https://s3.bmp.ovh/imgs/2021/12/5a70dee63635fcdc.png" />
+				厦门今日气温:${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C
+			</div>
+			<div style="font-size: 1rem;float:right;padding: 1rem 1rem;">
+				<div style="padding-bottom: 1rem;">
+					<img style="width: 6%;"; src="https://s3.bmp.ovh/imgs/2021/12/f4e0eace95359e80.png" />
+					${daily[1].name}(${daily[1].category}):
+					${daily[1].text}
+				</div>
+				<div style="padding-bottom: 1rem;">
+					<img style="width: 6%;"; src="https://s3.bmp.ovh/imgs/2021/12/678c04dd55d73f81.png" />
+					${daily[2].name}(${daily[2].category}):
+					${daily[2].text}
+				</div>
+				<div style="padding-bottom: 1rem;">
+					<img style="width: 6%;"; src="https://s3.bmp.ovh/imgs/2021/12/f63a46c72a12aca3.png" />
+					${daily[0].name}(${daily[0].category}):
+					${daily[0].text}
+				</div>
+           </div>
         </div>
       </div>
-    </body>
-  </html>
+      </body>
+    </html>
   `;
 }
 
