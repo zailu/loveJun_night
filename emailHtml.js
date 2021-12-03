e70c0392cd8076545976079b916ec9bc90eb0b76
zailu <919422364@qq.com>
2021-12-03 11:27:48 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -10,7 +10,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
-      <div style="font-size: 1rem;width: 100%;height: 100%;overflow-y: scroll;box-sizing: border-box;border:1px solid rgba(216, 131, 197, 0.5);">
+      <div style="font-size: 1rem;width: 100%;height: 100%;overflow-y: scroll;box-sizing: border-box;border:1px solid rgba(216, 131, 197, 0.5);position: relative;">
 			<!-- 天数 -->
 			<div style="font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;position: relative;color: #000000;margin-bottom: 2px;letter-spacing: 0.2rem;">
 				<!-- <p>今天是在一起的第${lovingDays}天！</p>-->
@@ -27,7 +27,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
 			<div style="float:left;font-size: 1rem;writing-mode:tb-lr;width: 60%;">
 				${content}
 			</div>
-			<div style="float: left;bottom: auto;">
+			<div style="position:absolute;bottom:0">
 				<img  style="width: 100%;"; src="https://s3.bmp.ovh/imgs/2021/12/c31203495969e9f1.png" />
 			</div>
       </div>
