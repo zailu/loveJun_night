49a7b319ad8608eb3f2cffdec869d770496a0cd5
zailu <919422364@qq.com>
2021-12-17 09:50:36 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -26,7 +26,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
 			<!-- 每日一句 -->
 			<div style="font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;float: left;color: #0000ff;margin-bottom: 2px;letter-spacing: 0.2rem;margin-top: 3rem;">
 				<!-- ${content} -->
-				珍惜今天，珍惜现在，谁知道明天和意外，哪一个先来。
+				双向奔赴让一切都变得有意义。
 			</div>
 			<div style="position:absolute;bottom:0">
 				<img  style="width: 100%;"; src="https://s3.bmp.ovh/imgs/2021/12/c31203495969e9f1.png" />
