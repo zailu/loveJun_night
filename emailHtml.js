1aa2064a041f55a79d12432950e948b7114dfc4b
zailu <919422364@qq.com>
2021-12-19 16:11:18 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -26,7 +26,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
 			<!-- 每日一句 -->
 			<div style="font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;float: left;color: #0000ff;margin-bottom: 2px;letter-spacing: 0.2rem;margin-top: 3rem;">
 				<!-- ${content} -->
-				你呀，你别再关心灵魂了，那是神明的事，你所能做的，是些小事情，诸如热爱时间，思念母亲，静悄悄地做人像早晨一样清白。
+				“我在，我会一直在，一直一直在”
 			</div>
 			<div style="position:absolute;bottom:0">
 				<img  style="width: 100%;"; src="https://s3.bmp.ovh/imgs/2021/12/c31203495969e9f1.png" />
