22f57c22d203051668bb694f7ee0d1c0c4baf3b0
zailu <919422364@qq.com>
2021-12-21 13:37:29 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -26,7 +26,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
 			<!-- 每日一句 -->
 			<div style="font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;float: left;color: #0000ff;margin-bottom: 2px;letter-spacing: 0.2rem;margin-top: 3rem;">
 				<!-- ${content} -->
-				我们一直寻找的， 却是自己原本早已拥有的；我们总是东张西望唯独漏了自己想要的，这就是我们至今难以如愿以偿的原因。
+				大雨中为你撑伞的人，黑暗中默默抱紧你的人。逗你笑的人，陪你彻夜聊天的人，坐车来看你的人，在医院陪你的人，陪你哭过的人，总是以你为重的人，是这些温暖使你成为善良的人。
 			</div>
 			<div style="position:absolute;bottom:0">
 				<img  style="width: 100%;"; src="https://s3.bmp.ovh/imgs/2021/12/c31203495969e9f1.png" />
