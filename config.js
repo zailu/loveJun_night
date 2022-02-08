4d00bbfdb7e815776508c7f47914cde9e05a1d36
zailu <919422364@qq.com>
2022-02-08 16:01:48 +0800

    LoveXuan

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // 配置信息
 module.exports = {
-  fromDisplayText: '崽崽', // 收件箱展示的来件人名字
+  fromDisplayText: 'Moonlight', // 收件箱展示的来件人名字
   fromDisplaySubText: '晚安', // 收件箱展示的次级标题
   user: '919422364@qq.com', // 发送者邮箱
   pass: 'xkhoaxouqlfjbbif', // 发送者邮箱MTP协议密码
-  to: '1594072713@qq.com', // 发送到谁，填邮箱
-  //to: '919422364@qq.com', // 发送到谁，填邮箱
+  //to: '1594072713@qq.com', // 发送到谁，填邮箱
+  to: '919422364@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-  location: '101230201',  // 和风天气-厦门的id
+  location: '101280604',  // 和风天气-厦门的id 101230201   深圳的id 101280604
   type: '1,3,9', // 和风天气-生活指数type
   tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
   startDay: '2016-10-12', // 在一起的日期
