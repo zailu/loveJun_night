e0a50763d4513b6ce4c7a7cc28c83b5e2210140e
jack_xue <919422364@qq.com>
2022-11-22 22:55:32 +0800

    test

--- emailHtml.js
+++ emailHtml.js
@@ -18,10 +18,10 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
 				<p>结束了一天的工作</p>
 				<div>
 					今天也要好好睡觉哦
-					<img  style="width: 8%;"; src="https://s3.bmp.ovh/imgs/2021/12/d82c0c74feafd80c.png" />
+					<img  style="width: 8%;"; src="https://s3.uuu.ovh/imgs/2022/11/22/58b28726161eda8b.png" />
 				</div>
 				<div style="float:right">
-					<img  style="width: 70%;"; src="https://s3.bmp.ovh/imgs/2021/12/bbb7a7a1173ec12e.png" />
+					<img  style="width: 70%;"; src="https://s3.uuu.ovh/imgs/2022/11/22/dfaa964d0f823936.png" />
 				</div>
 			</div>
 			<!-- 每日一句 -->
@@ -29,7 +29,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
 				 ${content} 			
 			</div>
 			<div style="position:absolute;bottom:0">
-				<img  style="width: 100%;"; src="https://s3.bmp.ovh/imgs/2021/12/c31203495969e9f1.png" />
+				<img  style="width: 100%;"; src="https://s3.uuu.ovh/imgs/2022/11/22/461cbb8ca7df3587.png" />
 			</div>
       </div>
       </body>
